1eb341715b44188def9b64a99e324d1d2b89c7d5
jasminhui <jasminqh@yahoo.com.hk>
2023-10-29 00:30:29 +0100

    inital R script

---
+++ transformation_function.R
@@ -0,0 +1,23 @@
+# Load tidyverse package
+library(tidyverse)
+
+# Define a function that performs input transformation on a dataset
+transform_function <- function(data) {
+  # Ensure that dataset contains the columns 'height' and 'age'
+  if("height" %in% colnames(data) & "age" %in% colnames(data)) {
+    # Use the 'mutate' function to calculate yearly growth
+    data <- data %>%
+      mutate(yearly_growth = height / age)
+    return(data)
+  } else {
+    stop("There is no suitable data in this dataset to calculate yearly growth.")
+  }
+}
+
+data(Loblolly)
+
+# Call function on 'loblolly' dataset
+Loblolly_transformed <- transform_function(Loblolly)
+
+# Print first few rows of transformed dataset
+head(Loblolly_transformed)
