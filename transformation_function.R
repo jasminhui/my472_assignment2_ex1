aeb5b5245d0e63715117c13cb0a40fdbf7ddc768
jasminhui <jasminqh@yahoo.com.hk>
2023-10-29 00:38:40 +0100

    testing branches

--- transformation_function.R
+++ transformation_function.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 
 # Define a function that performs input transformation on a dataset
-transform_function <- function(data) {
+transform_function <- function(data, type) {
   # Ensure that dataset contains the columns 'height' and 'age'
   if("height" %in% colnames(data) & "age" %in% colnames(data)) {
     # Use the 'mutate' function to calculate yearly growth
